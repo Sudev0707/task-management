1b46a2fe550da4b8a43a204f0428319da1470641
sudev <58890958+Sudev0707@users.noreply.github.com>
2025-12-18 15:11:15 +0530

    change

--- src/routes/Screens.tsx
+++ src/routes/Screens.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
-
 // export { default as LoginScreen } from '../screens/auth/LoginScreen';
 // export { default as SignUpScreen } from '../screens/auth/SignupScreen';
 // export { default as DashBoard } from '../screens/task/TaskDashboard';
 // export { default as EditTask } from '../screens/task/EditTask';
 // export { default as AddTask } from '../screens/task/AddTask';
 
-
-
 export const LoginScreen = React.lazy(() => import('../screens/auth/LoginScreen'));
 export const SignUpScreen = React.lazy(() => import('../screens/auth/SignupScreen'));
 export const DashBoard = React.lazy(() => import('../screens/task/TaskDashboard'));
 export const EditTask = React.lazy(() => import('../screens/task/EditTask'));
 export const AddTask = React.lazy(() => import('../screens/task/AddTask'));
+
+
+
+
+
